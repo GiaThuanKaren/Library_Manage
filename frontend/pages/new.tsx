ec371dff60a7c31c33c243e2a7c16ae006c0ffbd
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2023-04-16 16:32:00 +0700

    Add Create New Post Services

--- frontend/pages/new.tsx
+++ frontend/pages/new.tsx
@@ -7,6 +7,7 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import dynamic from "next/dynamic";
 import { CreateNewPost } from "src/service/api";
+import { url } from "inspector";
 interface Prop {
   text: string;
   handleFUNC: React.Dispatch<React.SetStateAction<string>>;
@@ -16,26 +17,53 @@ const Editor = dynamic(() => import("../src/components/Editor/index"), { ssr: fa
 
 const Edit: FC<Prop> = function ({ handleFUNC, text }) {
   const InputEle = React.useRef<any>();
-  const [ImageUrl, SetImageurl] = React.useState("");
+  const [ImageUrl, SetImageurl] = React.useState<{
+    url: string;
+    filesImage: File
+  }>();
 
   React.useEffect(() => { }, []);
   const handleCreatePost = async function () {
     try {
-      let result = await CreateNewPost()
+      let result = await CreateNewPost("telskdj", "sldifj", ImageUrl?.filesImage as File)
       console.log(result)
     } catch (e) {
       throw e
     }
   }
+  const HandleChooseCover = async function () {
+    try {
+
+
+    } catch (error) {
+      throw error
+    }
+  }
+
   return (
     <>
-      <div className="h-20">
-        <div className="h-full w-full">
-          <img src={ImageUrl} alt={ImageUrl} />
+
+      <div className="h-44">
+        <div className="h-full w-full overflow-hidden">
+          <img className="h-full w-full object-contain" src={ImageUrl?.url} alt={ImageUrl?.url} />
         </div>
       </div>
+      <div onClick={() => {
+        InputEle.current.click();
+      }} className="flex justify-end">
+        <p
+          className="cursor-pointer md:flex hidden hover:bg-blue-600 hover:text-white hover:font-medium  border-[2px] border-[#D4D4D4] px-3 py-2 rounded-md items-center justify-center"
+        >Choose Cover</p>
+      </div>
       <Editor onChange={() => { }} value={""} />
-      <input type="file" name="tenfile" id="" />
+      <input onChange={(e) => {
+        let files = e.target?.files?.item(0)
+        let link = URL.createObjectURL(files as Blob)
+        SetImageurl({
+          url: link,
+          filesImage: files as File
+        })
+      }} className="hidden" ref={InputEle} type="file" name="tenfile" id="" />
       <div onClick={handleCreatePost} className="flex justify-end">
         <p
           className="cursor-pointer md:flex hidden hover:bg-blue-600 hover:text-white hover:font-medium  border-[2px] border-[#D4D4D4] px-3 py-2 rounded-md items-center justify-center"
@@ -87,7 +115,7 @@ function CreateNew() {
   return (
     <>
       <Mainlayout>
-        <div className="flex items-center justify-center">
+        {/* <div className="flex items-center justify-center">
           {ArrTab.map((item: any, index: number) => {
             return (
               <>
@@ -105,7 +133,7 @@ function CreateNew() {
               </>
             );
           })}
-        </div>
+        </div> */}
         <div className="relative h-[70vh] overflow-y-auto">
           {chooseTab === "edit" ? (
             <Edit handleFUNC={SettextMarkDown} text={textMarkDown} />
--- frontend/src/service/api/index.ts
+++ frontend/src/service/api/index.ts
@@ -11,15 +11,24 @@ export const GetAllPost = async function () {
   }
 };
 
-export const CreateNewPost = async function () {
+export const CreateNewPost = async function (title: string, body: any, cover_image: File) {
   try {
-    let result = await axios.post(BASE_URL_PRO+"/createArticle", {
-      "title": "Test tile gia thuna 097",
-      "body": "There has been some buzz recently in the frontend world around the term \"Signals\". In seemingly short",
-      "cover_image": "https://res.cloudinary.com/practicaldev/image/fetch/s--hhnkUFvf--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/olad9yyw632rg959kewz.png",
-      "userId": "63ef7d4a571fc2238ed33d83"
+    let formdata = new FormData()
+    formdata.append("tenfile", cover_image)
+
+    let resultImageUpload = await axios.post("https://instagram-backend-gia-thuan.vercel.app/api/upload/upload_single", {
+      formdata
+    }, {
+      headers: { "Content-Type": "multipart/form-data", 'Access-Control-Allow-Origin': '*' },
     })
-    return MSG("Done", result.data, null)
+    return MSG("Done", resultImageUpload.data)
+    // let result = await axios.post(BASE_URL_PRO+"/createArticle", {
+    //   "title": "Test tile gia thuna 097",
+    //   "body": "There has been some buzz recently in the frontend world around the term \"Signals\". In seemingly short",
+    //   "cover_image": "https://res.cloudinary.com/practicaldev/image/fetch/s--hhnkUFvf--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/olad9yyw632rg959kewz.png",
+    //   "userId": "63ef7d4a571fc2238ed33d83"
+    // })
+    // return MSG("Done", result.data, null)
   } catch (e) {
     throw e
   }
